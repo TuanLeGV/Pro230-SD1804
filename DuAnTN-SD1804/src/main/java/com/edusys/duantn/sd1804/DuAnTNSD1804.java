56aa230611f375aeab9dd90d6dc3f542cd2c4ef4
TuanLeGV <tuanlegv2023@gmail.com>
2025-03-20 15:32:57 +0700

    Initial project

---
+++ DuAnTN-SD1804/src/main/java/com/edusys/duantn/sd1804/DuAnTNSD1804.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.edusys.duantn.sd1804;
+
+/**
+ *
+ * @author user
+ */
+public class DuAnTNSD1804 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
